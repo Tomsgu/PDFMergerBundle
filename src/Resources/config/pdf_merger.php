987b6a8844fba0ee25d993a7a9cb24ea5df6a778
Tomas <tomasjakll@gmail.com>
2026-01-14 19:54:26 +0100

    Add Symfony 8 support

--- src/Resources/config/pdf_merger.php
+++ src/Resources/config/pdf_merger.php
@@ -8,12 +8,18 @@ use Tomsgu\PdfMerger\PdfMerger;
 return static function (ContainerConfigurator $container) {
     $services = $container->services();
 
+    $fpdiServiceId = 'tomsgu_pdf_merger.fpdi';
+    // BC for symfony < 5.0
+    $fpdiService =  function_exists(__NAMESPACE__ . '\service')
+        ? service($fpdiServiceId)
+        : ref($fpdiServiceId);
+
     $services->set('tomsgu_pdf_merger.pdf_merger', PdfMerger::class)
-        ->args([service('tomsgu_pdf_merger.fpdi')])
+        ->args([$fpdiService])
         ->private()
     ;
 
-    $services->set('tomsgu_pdf_merger.fpdi', Fpdi::class)
+    $services->set($fpdiServiceId, Fpdi::class)
         ->private()
     ;
 };
